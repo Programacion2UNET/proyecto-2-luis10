1cefd0672d21640d3e4c438b2d585d2832420b17
ojoblanco <alexander171296@gmail.com>
2018-02-22 15:52:18 -0430

    agregando nuevos archvos y cambiando css de la portada

---
+++ acceso_admin.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+       <style type="text/css">
+		 
+		 html{
+   	 	   background-color: green;
+   	     }
+		 #portada{
+   	 	 width:50%;  
+   	 	 background-color: orange;
+   	    }
+		 .accseso{
+	
+			 text-decoration: none;
+			 font-size: 30px;
+			 color: white;
+		 }
+		 li{
+			 list-style:none;
+		}
+		 div{
+			text-align: center;
+			 position:absolute;
+			 left: 25%;
+		 }
+		 a:hover{
+			color: black;
+		 }
+			 
+		  
+         h1{
+      	  text-align: center;
+      	  color: white;
+      	  font-family: cursive;
+         }
+
+		</style>
+</head>
+
+<body>
+
+    <h1>Bienvenido al Menu de Administrador</h1>
+      <div id="portada">
+      	
+      	   <ul class="menu">
+      	   	   <li><a class="accseso" href="#">listado con todos los equipos inscritos en el torneo</a></li>
+      	   	   <li><a class="accseso" href="#">Detalles de una inscripcion</a></li>
+               <li><a class="accseso" href="#">Editar un registro</a></li>
+      	   	   <li><a class="accseso" href="#">borrar un registro</a></li>
+               <li><a class="accseso" href="#">Registrar torneos definir la fecha de realización</a></li>
+      	   </ul>
+
+      </div>
+     
+
+</body>
+</html>
---
+++ registro_torneo.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Resgistro torneo</title>
+   <style type="text/css">
+		 
+		 html{
+   	 	   background-color: green;
+   	     }
+   	     .formulario{
+   	     	 position: absolute;
+   	     	 left: 40%;
+   	     	 top: 10%;
+   	     }
+         label{
+         	color: white;
+         	font-size: 24px; 
+         }
+         h1{
+      	  text-align: center;
+      	  color: white;
+      	  font-family: cursive;
+         }
+
+		</style>
+</head>
+
+<body>
+
+     <h1>Registro de Torneo</h1>
+
+
+</body>
+</html>
